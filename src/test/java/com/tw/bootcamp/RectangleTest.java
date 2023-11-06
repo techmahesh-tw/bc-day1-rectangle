0da953c5cd91900ff3c7e0a3880b628341735446
maheshkumarsellamuthu <maheshkumar.s@thoughtworks.com>
2023-11-06 15:27:33 +0530

    Add - refactored.

--- src/test/java/com/tw/bootcamp/RectangleTest.java
+++ src/test/java/com/tw/bootcamp/RectangleTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
- class RectangleTest {
+class RectangleTest {
 
     @Test
     void testCalculateRectangleAreaNegativeInput() {
@@ -16,10 +16,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
     void testCalculateRectangleAreaNullInput() {
         assertThrows(IllegalArgumentException.class, () -> new Rectangle(null, null));
     }
-     @Test
-     void testCalculateRectangleArea() {
-         Rectangle rectangle = new Rectangle(2.0f, 3.0f);
-         assertEquals(6.0f, rectangle.area());
-     }
+
+    @Test
+    void testCalculateRectangleArea() {
+        Rectangle rectangle = new Rectangle(2.0f, 3.0f);
+        assertEquals(6.0f, rectangle.area());
+    }
 
 }
