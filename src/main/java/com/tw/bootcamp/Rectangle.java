fecbf0a45efa130104e6d6e9d8f85d80844884df
maheshkumarsellamuthu <maheshkumar.s@thoughtworks.com>
2023-11-06 14:37:08 +0530

    Add - refactored.

--- src/main/java/com/tw/bootcamp/Rectangle.java
+++ src/main/java/com/tw/bootcamp/Rectangle.java
@@ -3,18 +3,11 @@ package com.tw.bootcamp;
 public class Rectangle {
 
     public Float calculate(Float length, Float breath) {
-        if (validateInput(length, breath)) {
-            return length * breath;
-        }
-        return null;
-    }
-
-    private boolean validateInput(Float length, Float breath) {
         if (null == length && null == breath) {
             throw new IllegalArgumentException("Inputs should not be null");
         } else if (length <= 0.0f || breath <= 0.0f) {
             throw new IllegalArgumentException("Inputs have to be positive numbers");
         }
-        return true;
+        return length * breath;
     }
 }
