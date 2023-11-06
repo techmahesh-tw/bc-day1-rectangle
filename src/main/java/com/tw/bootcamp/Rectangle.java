c02fc29686ac88ee7bce1a4d76ef2dddedf97004
Nitish Deshpande <nitish.deshpande@Nitishs-MBP.bbrouter>
2023-11-06 15:15:02 +0530

    Update validation of input params to Rectangle

--- src/main/java/com/tw/bootcamp/Rectangle.java
+++ src/main/java/com/tw/bootcamp/Rectangle.java
@@ -6,7 +6,7 @@ public class Rectangle {
     private Float breath;
 
     public Rectangle(Float length, Float breath) {
-        if (null == length && null == breath) {
+        if (null == length || null == breath) {
             throw new IllegalArgumentException("Inputs should not be null");
         } else if (length <= 0.0f || breath <= 0.0f) {
             throw new IllegalArgumentException("Inputs have to be positive numbers");
