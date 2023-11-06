3700d8428c5594574e5a068b27a4cadd4a865701
Nitish Deshpande <nitish.deshpande@Nitishs-MBP.bbrouter>
2023-11-06 15:09:01 +0530

    Update Rectangle domain class

--- src/main/java/com/tw/bootcamp/Rectangle.java
+++ src/main/java/com/tw/bootcamp/Rectangle.java
@@ -2,12 +2,20 @@ package com.tw.bootcamp;
 
 public class Rectangle {
 
-    public Float area(Float length, Float breath) {
+    private Float length;
+    private Float breath;
+
+    public Rectangle(Float length, Float breath) {
         if (null == length && null == breath) {
             throw new IllegalArgumentException("Inputs should not be null");
         } else if (length <= 0.0f || breath <= 0.0f) {
             throw new IllegalArgumentException("Inputs have to be positive numbers");
         }
+        this.length = length;
+        this.breath = breath;
+    }
+
+    public Float area() {
         return length * breath;
     }
 }
--- src/test/java/com/tw/bootcamp/RectangleTest.java
+++ src/test/java/com/tw/bootcamp/RectangleTest.java
@@ -8,21 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  class RectangleTest {
 
     @Test
-    void testCalculateRectangleArea() {
-        Float result = new Rectangle().area(2.0f, 3.0f);
-        assertEquals(6.0f, result);
-    }
-
-    @Test
     void testCalculateRectangleAreaNegativeInput() {
-        Rectangle rectangle = new Rectangle();
-        assertThrows(IllegalArgumentException.class, () -> rectangle.area(-2.0f, 3.0f));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(-2.0f, 3.0f));
     }
 
     @Test
     void testCalculateRectangleAreaNullInput() {
-        Rectangle rectangle = new Rectangle();
-        assertThrows(IllegalArgumentException.class, () -> rectangle.area(null, null));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(null, null));
     }
+     @Test
+     void testCalculateRectangleArea() {
+         Rectangle rectangle = new Rectangle(2.0f, 3.0f);
+         assertEquals(6.0f, rectangle.area());
+     }
 
 }
